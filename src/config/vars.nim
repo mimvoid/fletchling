782a581972e57eb93d091434ee777f2eac8e4806
mimvoid <mimvoid@proton.me>
2024-12-20 20:44:01 -0500

    add basic config variables

---
+++ src/config/vars.nim
@@ -0,0 +1,35 @@
+from std/os import fileExists
+from std/appdirs import getConfigDir
+import std/[strutils, paths, parsecfg]
+
+
+# Default settings
+var
+  noColor* = false
+  nerdFont* = true
+  showArt* = true
+  overrideArt* = false
+  customArt* = ""
+
+
+func getVal[T](cfg: Config, section, key: string, parser: proc, default: T): T =
+  let val = cfg.getSectionValue(section, key)
+
+  try:
+    return parser(val)
+  except ValueError:
+    return default
+
+
+# Get config file settings
+let configFile = $getConfigDir().absolutePath & "fletchling/config.ini"
+
+if configFile.fileExists():
+  let cfg = loadConfig(configFile)
+
+  noColor = cfg.getVal("", "noColor", parseBool, noColor)
+  nerdFont = cfg.getVal("", "nerdFont", parseBool, nerdFont)
+
+  showArt = cfg.getVal("Art", "showArt", parseBool, showArt)
+  overrideArt = cfg.getVal("Art", "overrideArt", parseBool, overrideArt)
+  customArt = cfg.getSectionValue("Art", "customArt", customArt)
